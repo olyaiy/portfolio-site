2b394b404577f0692de656a997fd8c257c783cc8
olyaiy <alexolyaiy@gmail.com>
2024-08-25 15:14:11 -0700

    add homepage bg and heading

--- src/components/home-page.tsx
+++ src/components/home-page.tsx
@@ -3,7 +3,13 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+
+      <div className="flex flex-col">
+        <h1>Hi, I'm Alex 👋</h1>
+
+
+      </div>
+
     </div>
   )
 }
